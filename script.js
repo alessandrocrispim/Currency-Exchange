ee51970a662e935c8e2da60d5cb7fab13351c1af
Alessandro Alves Crispim <alessandro.crispim.1979@gmail.com>
2021-09-20 17:58:36 -0300

    .

--- script.js
+++ script.js
@@ -1,25 +1,57 @@
 const usdInput = document.getElementById('USD');
 const brlInput = document.getElementById('BRL');
 
-const submitButton = document.getElementById('submit');
+/*const submitButton = document.getElementById('submit');*/
 
 const cotation = 5.22;
 
-submitButton.addEventListener('click', event => { 
-    console.log ('botao');
+function replaceComma(string) {
+    return string.replace(',', '.');
+}
 
-    let value = usdInput.value;
 
-    brlInput.value = (value * cotation).toFixed(2);
+usdInput.addEventListener( 'input', (event) => {
     
 
-})
+    let value = Number(
+        replaceComma(event.target.value)
+        );
+
+        fetch('https://api.coinbase.com/v2/exchange-rates?currency=USD')
+
+        .then(response => {
+            return response.json();
+        })
+
+        .then(data => {
+            
+            brlInput.value = (value * data.data.rates.BRL).toFixed(2);
+        });
+
+   
+});
+
+brlInput.addEventListener( 'input', (event) => {
+
+    let value = Number(
+        replaceComma(event.target.value)
+        );
+
+        fetch('https://api.coinbase.com/v2/exchange-rates?currency=USD')
+
+        .then(response => {
+            return response.json();
+        })
+
+        .then(data => {
+            
+            usdInput.value = (value / data.data.rates.BRL).toFixed(2);
+        });
+
+});
 
 
 
-console.log(usdInput);
-console.log(brlInput);
-console.log(submitButton);
 
 /*request e response => estudos de requisicao http
 consumir api 
